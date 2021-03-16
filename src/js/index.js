0e4b247d71390f15217ef6fa0d1d5c9b1430ef5a
신혁준 <67365409+SHJ-9712@users.noreply.github.com>
2021-03-16 21:33:59 +0900

    index 금융서비스 섹션 완성

--- src/js/index.js
+++ src/js/index.js
@@ -15,3 +15,19 @@ $('.main-slider').slick({
     vertical: false,
     draggable: true,
 });
+// slick 금융서비스 추천 금융상품
+$('.product-list').slick({
+    slide: '.product-box',
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: 'linear',
+    arrows: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    vertical: false,
+    draggable: true,
+});
