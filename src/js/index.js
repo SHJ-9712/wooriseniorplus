f9d6de98f24594f3e32787010cf27c28154bd9e9
신혁준 <67365409+SHJ-9712@users.noreply.github.com>
2021-04-16 19:43:05 +0900

    Add quick menu

--- src/js/index.js
+++ src/js/index.js
@@ -41,6 +41,38 @@ $('.section_2 .tab > li > button').click(function(){
     $(this).addClass('active').siblings('.tab-list').show();
 })
 
+// 퀵메뉴
+var quickList = $('.quick-menu > ul > li');
+var contents = $('.main .quick-stc');
+var header_h = $('header').outerHeight();
+quickList.click(function(e){
+    e.preventDefault();
+    var target = $(this);
+    var idx = target.index();
+    var section = contents.eq(idx);
+    var sec_pos = parseInt(section.offset().top);
+    if(idx == 0) {
+        $('html, body').stop().animate({scrollTop: 0});
+    } else {
+        $('html, body').stop().animate({scrollTop: sec_pos - header_h});
+    }
+});
+$(window).scroll(function(){
+    var st = $(window).scrollTop();
+    contents.each(function(){
+        var target = $(this);
+        var idx = target.index();
+        var pos = parseInt(target.offset().top) - header_h;
+        if(pos <= st) {
+            quickList.removeClass('on');
+            quickList.eq(idx).addClass('on');
+        } else if(idx == 0) {
+            quickList.removeClass('on');
+            quickList.eq(0).addClass('on');
+        }
+    });
+});
+
 // happycall 약관보기 모달
 $('.happycall .check > .btn-policy').click(function(){
     $('.policy').show();
